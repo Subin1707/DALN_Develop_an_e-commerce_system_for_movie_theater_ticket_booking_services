21fcd1116057ba0ac68bfa076016804315a3630e
sonlethai2810 <sonlt2810@gmail.com>
2026-04-15 17:50:23 +0700

    Thiết kế model movie

---
+++ app/Models/Movie.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Movie extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'title',
+        'description',
+        'genre',
+        'duration',
+        'poster',
+        'release_date',
+        'status',
+    ];
+
+    public function showtimes()
+    {
+        return $this->hasMany(Showtime::class);
+    }
+    public function comments()
+    {
+        return $this->hasMany(Comment::class, 'movies_id');
+    }
+}
